75bf40602177da47ae4669d58321efec84992a1e
SultanovRuslan86 <sulrus1986@gmail.com>
2023-07-17 19:12:18 +0200

    Add pages

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from schemas import Book
+
+from typing import List
 
 app = FastAPI()
 
@@ -8,12 +10,14 @@ def create_book(item: Book):
     return item
 
 
-@app.get('/book')
-def get_book(q: str = Query(None, min_length=2, max_length=10, description='Search book')):
-    return q
+# @app.get('/book')
+# def get_book(q: str = Query(None, min_length=2, max_length=10, description='Search book')):
+#     return q
 
 
-
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int=Query(None, gt=10, le=500)):
+    return {'pk': pk, 'pages': pages}
 
 
 
