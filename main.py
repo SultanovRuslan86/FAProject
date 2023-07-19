7869dbcda6353a89e0b5cd679b3b701d1b932990
SultanovRuslan86 <sulrus1986@gmail.com>
2023-07-19 19:53:38 +0200

    Add pages

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Query, Path
-from schemas import Book
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 from typing import List
 
 app = FastAPI()
 
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    return {'item': item, 'author': author, 'quantity': quantity}
 
 
 # @app.get('/book')
@@ -19,6 +19,15 @@ def create_book(item: Book):
 def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int=Query(None, gt=10, le=500)):
     return {'pk': pk, 'pages': pages}
 
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {'author': author}
+
+
+@app.post('/books', response_model=Book, response_model_exclude_unset=False)
+def create_book(item: Book):
+    return item
 
 
 
--- schemas.py
+++ schemas.py
@@ -1,9 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, field_validator, Field
 from datetime import date
 from typing import List
 
-class Genres(BaseModel):
+class Genre(BaseModel):
     name: str
+
+
+class Author(BaseModel):
+    first_name: str = Field(..., max_length=25)
+    last_name: str
+    age: int = Field(..., gt=15, lt=90,
+                     description='Author age must be more than 15 years and not more then 90 years!'
+                     )
+
+    # @field_validator('age')
+    # def check_age(cls, v):
+    #     if v < 15 > 90:
+    #         raise ValueError('Author age must be more than 15 years and not more then 90 years!')
+    #     return v
+
+
 
 class Book(BaseModel):
     title: str
@@ -11,5 +27,10 @@ class Book(BaseModel):
     duration: str
     date: date
     summary: str
-    genres: List[Genres]
+    genres: List[Genre] = []
     pages: int
+
+
+
+
+
